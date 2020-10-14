e7daa58f53fdf71cfc85a5b83ccc37d679680765
Nathalia Lanzendorf <nathalia.lanzendorf@gmail.com>
2020-10-14 20:25:16 -0300

    Create the project

---
+++ src/main/java/com/lanzendorf/bellaFace/BellaFaceApplication.java
@@ -0,0 +1,13 @@
+package com.lanzendorf.bellaFace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BellaFaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BellaFaceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lanzendorf/bellaFace/BellaFaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lanzendorf.bellaFace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BellaFaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
